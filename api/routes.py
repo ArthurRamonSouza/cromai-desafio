b5b64e21df2a139f42164d39224e4949f09b37a5
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-01-23 16:07:40 -0300

    API finalizada.

--- api/routes.py
+++ api/routes.py
@@ -4,18 +4,33 @@ import main
 # Criando o site da API e a API com Flask 
 app = Flask("Desafio Cromai API")
 
+# Por padrão o Flask ordena o json de resposta
+#app.config["JSON_SORT_KEYS"] = False
+
 @app.route("/", methods=["GET"])
 def homePage():
     return {"Pagina": "inicial"}
     # return {"key": "value"}
 
-@app.route("/api", methods=["GET"])
+@app.route("/teorema", methods=["GET"])
 def recebeCatetos():
-    # Pegar os parametros da requisição e armazená-los
-    ladoA = False
-    ladoB = False
-    ladoC = False
 
+    # Pegar os parametros da requisição, verificar se é nulo ou não e depois armazená-los
+    if(request.args.get("ladoa") != None):
+        ladoA = float(request.args.get("ladoa"))
+    else:
+        ladoA = False
+    
+    if(request.args.get("ladob") != None):
+        ladoB = float(request.args.get("ladob"))
+    else:
+        ladoB = False
+    
+    if(request.args.get("ladoc") != None):
+        ladoC = float(request.args.get("ladoc"))
+    else:
+        ladoC = False
+    
     # Caso não haja erros na requisição, o cáculo do teorema será aplicado
     if((ladoA and ladoB) or (ladoA and ladoC) or (ladoB and ladoC)):
         status = 200
