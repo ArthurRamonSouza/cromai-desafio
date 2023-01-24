f548cff037007f7e1c32a5fef3b884a77f726f27
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-01-24 13:58:38 -0300

    Algumas alterações e iniciando o site da API.

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import main
 
 # Criando o site da API e a API com Flask 
@@ -9,7 +9,7 @@ app = Flask("Desafio Cromai API")
 
 @app.route("/", methods=["GET"])
 def homePage():
-    return {"Pagina": "inicial"}
+    return render_template("guide.html")
     # return {"key": "value"}
 
 @app.route("/teorema", methods=["GET"])
@@ -17,17 +17,29 @@ def recebeCatetos():
 
     # Pegar os parametros da requisição, verificar se é nulo ou não e depois armazená-los
     if(request.args.get("ladoa") != None):
-        ladoA = float(request.args.get("ladoa"))
+        try:
+            ladoA = float(request.args.get("ladoa"))
+        except:
+            mensagem = "Valor do parâmetro 'ladoa' inválido."
+            ladoA = False
     else:
         ladoA = False
     
     if(request.args.get("ladob") != None):
-        ladoB = float(request.args.get("ladob"))
+        try:
+            ladoB = float(request.args.get("ladob"))
+        except:
+            mensagem = "Valor do parâmetro 'ladob' inválido."
+            ladoB = False
     else:
         ladoB = False
     
     if(request.args.get("ladoc") != None):
-        ladoC = float(request.args.get("ladoc"))
+        try:
+            ladoC = float(request.args.get("ladoc"))
+        except:
+            mensagem = "Valor do parâmetro 'ladoc' inválido."
+            ladoC = False
     else:
         ladoC = False
     
@@ -39,11 +51,11 @@ def recebeCatetos():
         return response(status, mensagem, resultado)
 
     # Caso dois ou mais dos lados sejam nulos, retorna um erro
-    status = 405
-    mensagem = "Requisicao invalida!"
+    status = 400
+    if(mensagem is None):
+        mensagem = "Requisicao invalida!"
 
     return response(status, mensagem)
-
 
 def response(status, mensagem, resultado = False):
     response = {}
