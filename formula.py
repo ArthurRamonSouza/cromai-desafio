7c9192ec4f945a46797bb748205ca8b12d1eafc2
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-01-25 18:35:59 -0300

    Código corrigido

--- formula.py
+++ formula.py
@@ -1,70 +1,19 @@
-from flask import Flask, request, render_template
-import formula
+import math
 
-# Criando a API com Flask 
-app = Flask(__name__)
+# Função que será chamada para calcular o resultado segundo o teorema de Pitágoras
+def pitagoras(ladoA = False, ladoB = False, ladoC = False):
+    if(ladoA and ladoB):
+        ladoC  = math.sqrt(math.pow(ladoA, 2) + math.pow(ladoB, 2))
 
-# Por padrão o Flask ordena o json de resposta
-#app.config["JSON_SORT_KEYS"] = False
+    elif(ladoA and ladoC):
+        ladoB = math.sqrt(math.pow(ladoC, 2) - math.pow(ladoA, 2))
 
-# Mapeamento da API
-@app.get("/teorema")
-def recebeCatetos():
-    mensagem = None
+    #elif(ladoB and ladoC):
+    #    ladoA = math.sqrt(math.pow(ladoC, 2) - math.pow(ladoB, 2))
 
-    # Pegar os parametros da requisição, verificar se é nulo ou não e depois armazená-los
-    if(request.args.get("ladoa") != None):
-        try:
-            ladoA = float(request.args.get("ladoa"))
-        except:
-            mensagem = "Valor do parametro 'ladob' invalido."
-            ladoA = False
     else:
-        ladoA = False
-    
-    if(request.args.get("ladob") != None):
-        try:
-            ladoB = float(request.args.get("ladob"))
-        except:
-            mensagem = "Valor do parametro 'ladob' invalido."
-            ladoB = False
-    else:
-        ladoB = False
-    
-    if(request.args.get("ladoc") != None):
-        try:
-            ladoC = float(request.args.get("ladoc"))
-        except:
-            mensagem = "Valor do parametro 'ladob' invalido."
-            ladoC = False
-    else:
-        ladoC = False
-    
-    # Caso não haja erros na requisição, o cáculo do teorema será aplicado
-    if((ladoA and ladoB) or (ladoA and ladoC) or (ladoB and ladoC)):
-        status = 200
-        mensagem = "Requisicao valida."
-        resultado = formula.pitagoras(ladoA, ladoB, ladoC)
-        return response(status, mensagem, resultado)
-
-    # Caso dois ou mais dos lados sejam nulos, retorna um erro
-    status = 400
-    if(mensagem is None):
-        mensagem = "Requisicao invalida!"
-
-    return response(status, mensagem)
-
-# Função para uma response personalizada e organizada
-def response(status, mensagem, resultado = False):
-    response = {}
-    response["Status"] = status
-    response["Mensagem"] = mensagem
-
-    # Se não tive erros na requisição, haverá um resultado
-    if(resultado):
-        response["Resultado"] = resultado
-
-    return response
-
-if __name__ == '__main__':
-    app.run()
+         ladoA = math.sqrt(math.pow(ladoC, 2) - math.pow(ladoB, 2))
+         
+    return {"Cateto A": ladoA,
+            "Cateto B": ladoB,
+            "Hipotenusa C": ladoC}
